e0606979f03d2b650f8b0dbd1f62c8f454eff172
xiaojiawang <xiao.jiawang@biaotutech.com>
2017-07-25 12:03:03 +0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  config.assets.precompile += %w( simditor.js simditor.css )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
